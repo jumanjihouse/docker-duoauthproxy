3d30a5b79bc65a443de7b3d5b55b7f00cdf4274d
Paul Morgan <jumanjiman@gmail.com>
2015-12-13 21:01:59 -0500

    harden the image

---
+++ runtime/harden.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+set -x
+set -e
+#
+# Docker build calls this script to harden the image during build.
+#
+# NOTE: To build on CircleCI, you must take care to keep the `find`
+# command out of the /proc filesystem to avoid errors like:
+#
+#    find: /proc/tty/driver: Permission denied
+#    lxc-start: The container failed to start.
+#    lxc-start: Additional information can be obtained by \
+#        setting the --logfile and --logpriority options.
+
+# Remove existing crontabs, if any.
+rm -fr /var/spool/cron
+rm -fr /etc/crontabs
+rm -fr /etc/periodic
+
+# Remove all but a handful of admin commands.
+find /sbin /usr/sbin ! -type d \
+  -a ! -name nologin \
+  -delete
+
+sysdirs="
+  /bin
+  /etc
+  /lib
+  /sbin
+  /opt
+  /usr
+"
+
+# Remove world-writable permissions.
+# Normally, I'd do this on the root fs, but circle ci fails with:
+#   chmod: /dev/mqueue: Operation not permitted
+# Therefore restrict the find to sysdirs listed above.
+find $sysdirs -xdev -type d -perm +0002 -exec chmod o-w {} +
+find $sysdirs -xdev -type f -perm +0002 -exec chmod o-w {} +
+
+# Remove apk configs.
+find $sysdirs -xdev -regex '.*apk.*' -exec rm -fr {} +
+
+# Remove crufty...
+#   /etc/shadow-
+#   /etc/passwd-
+#   /etc/group-
+find $sysdirs -xdev -type f -regex '.*-$' -exec rm -f {} +
+
+# Ensure system dirs are owned by root and not writable by anybody else.
+find $sysdirs -xdev -type d \
+  -exec chown root:root {} \; \
+  -exec chmod 0755 {} \;
+
+# Remove all suid files.
+find $sysdirs -xdev -type f -a -perm +4000 -delete
+
+# Remove other programs that could be dangerous.
+find $sysdirs -xdev \( \
+  -name hexdump -o \
+  -name chgrp -o \
+  -name chmod -o \
+  -name chown -o \
+  -name ln -o \
+  -name od -o \
+  -name strings -o \
+  -name su \
+  \) -delete
+
+# Remove init scripts since we do not use them.
+rm -fr /etc/init.d
+rm -fr /lib/rc
+rm -fr /etc/conf.d
+rm -fr /etc/inittab
+rm -fr /etc/runlevels
+rm -fr /etc/rc.conf
+
+# Remove kernel tunables since we do not need them.
+rm -fr /etc/sysctl*
+rm -fr /etc/modprobe.d
+rm -fr /etc/modules
+rm -fr /etc/mdev.conf
+rm -fr /etc/acpi
+
+# Remove root homedir since we do not need it.
+rm -fr /root
+
+# Remove fstab since we do not need it.
+rm -f /etc/fstab
+
+# Remove broken symlinks (because we removed the targets above).
+find $sysdirs -xdev -type l -exec test ! -e {} \; -delete
+
+# Remove unnecessary user accounts.
+sed -i -r '/^(duo)/!d' /etc/group
+sed -i -r '/^(duo)/!d' /etc/passwd
+
+# Remove interactive login shell for everybody but unprivileged user.
+sed -i -r '/^duo:/! s#^(.*):[^:]*$#\1:/sbin/nologin#' /etc/passwd
