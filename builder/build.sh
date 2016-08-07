366bd6f729991bf931f59b459491f3eaa5cd04fc
Paul Morgan <jumanjiman@gmail.com>
2016-08-07 15:56:35 +0000

    update to pyopenssl 16.0.0
    https://github.com/hynek/pyopenssl/blob/b62041bdf17/CHANGELOG.rst
    
    The update to python-six is necessary to avoid traceback with error:
    
        ImportError: cannot import name indexbytes

--- builder/build.sh
+++ builder/build.sh
@@ -9,10 +9,23 @@ cd /root
 tar xzf duoauthproxy-*-src.tgz
 cd duoauthproxy*
 patch -p0 < /root/config.patch
+
 pushd pkgs
-curl -L -o pyopenssl.tgz https://github.com/pyca/pyopenssl/archive/0.15.1.tar.gz
+# Erase bundled dirs.
+rm -fr pyopenssl*
+rm -fr six*
+
+# Download and extract new bundles.
+curl -L -o pyopenssl.tgz https://github.com/pyca/pyopenssl/archive/16.0.0.tar.gz
 tar xzf pyopenssl.tgz
+
+# python-six extracts by default as gutworth-six-<hash>.
+curl -L -o six.tgz https://bitbucket.org/gutworth/six/get/1.10.0.tar.gz
+six_dir='six-1.10.0/'
+mkdir ${six_dir}
+tar xzf six.tgz -C ${six_dir} --strip-components=1
 popd
+
 export PYTHON=$(which python)
 make
 cd duoauthproxy-build
